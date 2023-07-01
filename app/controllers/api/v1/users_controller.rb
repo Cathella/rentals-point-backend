b190a1da380b4f3d814c87c524c2b76b9d37cc99
Cathella <ncathie214@gmail.com>
2023-07-01 17:02:09 +0300

    Add jwt, add login route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,5 @@
+require 'jwt'
+
 module Api
   module V1
     class UsersController < ApplicationController
@@ -11,10 +13,41 @@ module Api
         end
       end
 
+      def login
+        user = User.find_by(email: params[:email])
+
+        if user && user.authenticate(params[:password])
+          # Generate a token (e.g., using JWT) and return it to the client for authentication
+          token = generate_token(user)
+          render json: { token: token }
+        else
+          render json: { error: 'Invalid email or password' }, status: :unauthorized
+        end
+      end
+
       private
 
       def user_params
         params.require(:user).permit(:username, :email, :password, :account)
+      end
+
+      def generate_token(user)
+        # Implement your token generation logic here (e.g., using JWT)
+        # Return the generated token
+
+        # Define your secret key for signing the token
+        secret_key = Rails.application.secrets.secret_key_base
+
+        # Define the expiration time for the token (e.g., 1 hour from now)
+        expiration_time = 1.hour.from_now.to_i
+
+        # Build the payload containing user information
+        payload = { user_id: user.id, exp: expiration_time }
+
+        # Generate the token using JWT
+        token = JWT.encode(payload, secret_key, 'HS256')
+
+        token
       end
     end
   end
